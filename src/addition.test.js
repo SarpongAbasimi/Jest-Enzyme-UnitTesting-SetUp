498bf2b9450080b698e96dc8806e17c9f5c3e921
sarpong <abasimisarpong@rocketmail.com>
2018-11-30 16:53:53 +0000

    just added a comment

--- src/addition.test.js
+++ src/addition.test.js
@@ -1,9 +1,9 @@
 import addition from './index';
 //To run jest in terminal just tupe in npm test
 
-/*test('myaddition',()=>{
+test('myaddition',()=>{
     expect(addition(4,6)).toBe(10);
-});*/
+});
 
 
 describe('My Addition',()=>{
