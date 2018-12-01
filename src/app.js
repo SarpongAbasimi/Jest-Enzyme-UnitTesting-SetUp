8056e5379186954298f5011e016a3ca296df182d
sarpong <abasimisarpong@rocketmail.com>
2018-12-01 01:11:28 +0000

    deleted some files in src folder

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class App extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <p>This is my first Test</p>
+        );
+    }
+}
+
+
+export default App
